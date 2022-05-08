e1a8edbaca4ce08fe2be5f366f326561b5a4e885
zhydani <zhydani@gmail.com>
2022-05-08 10:32:01 -0300

    menu

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,46 +1,59 @@
 <template>
-    <nav class="h-screen flex flex-col">
-        <nuxt-link to="/Inicio">
-            <h2 class="border-b p-5 text-xl">Inicio</h2>
-        </nuxt-link>
-        <div class="p-3 text-lg flex flex-col mb-4 overflow-y-scroll">
+  <v-card
+    class="mx-auto"
+    width="400"
+  >
+  <!--ONDE PEGAR OS ICONES: https://materialdesignicons.com/ -->
+    <v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
 
-            <nuxt-link to="/ValorPrevio">Valores Previos</nuxt-link>
-            <nuxt-link to="/Emolumentos">Emolumentos</nuxt-link>
-            <nuxt-link to="/Apresentante">Apresentantes</nuxt-link>
-            <nuxt-link to="/Ato">Atos</nuxt-link>
-            <nuxt-link to="/Cartorio">Cartorios</nuxt-link>
-            <nuxt-link to="/Comarca">Cidades</nuxt-link>
-            <nuxt-link to="/Circunscricao">Circunscricões</nuxt-link>
-            <nuxt-link to="/Corregedoria">Corregedoria</nuxt-link>
-            <nuxt-link to="/Devolucao">Devoluções</nuxt-link>
-            <nuxt-link to="/Especialidade">Especialidades</nuxt-link>
-            <nuxt-link to="/Estado">Estados</nuxt-link>
-            <nuxt-link to="/Indenizacao">Indenizações</nuxt-link>
-            <nuxt-link to="/MotivoDevolucao">Motivos de Devoluções</nuxt-link>
-            <nuxt-link to="/Protocolo">Protocolos</nuxt-link>
-            <nuxt-link to="/Status">Status</nuxt-link>
-            <nuxt-link to="/Titular">Titulares</nuxt-link>
-        </div>
-    </nav>
+        <nuxt-link to="/">Início</nuxt-link>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-currency-usd</v-icon>
+        </v-list-item-icon>
+
+        <nuxt-link to="/ValorPrevio">Valores Previos</nuxt-link>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-currency-usd</v-icon>
+        </v-list-item-icon>
+
+        <nuxt-link to="/Emolumentos">Emolumentos</nuxt-link>
+      </v-list-item>
+
+      
+    </v-list>
+  </v-card>
 </template>
 
 <script>
-export default {
-    name: 'SideBar',
-    data() {
-        return {}
-    },
-    computed: {
-       
-    },
-    methods: {
-    },
-}
+  export default {
+    data: () => ({
+      admins: [
+        ['Management', 'mdi-account-multiple-outline'],
+        ['Settings', 'mdi-cog-outline'],
+      ],
+      cruds: [
+        ['Create', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete'],
+      ],
+    }),
+  }
 </script>
 
+
 <style scoped>
-nav {
+.nav {
     background-color: #333533;
 }
 nav {
